4bfa5cb9caa8790f942791d1948ff878b8eb381b
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2023-01-21 16:09:34 +0900

    complete make code file demo

--- app/src/routes/Makefile.tsx
+++ app/src/routes/Makefile.tsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 0px 20px;
 `;
 
-const Codebox = styled.input`
-  width: 500px;
-  height: 500px;
-`;
+const Codebox = styled.textarea``;
 
 const Button = styled.button``;
 
 function Makefile() {
   const [value, setValue] = useState("");
   const [selectedOption, setSelectedOption] = useState<String>();
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     // const value = event.currentTarget.value;
     const {
       currentTarget: { value }
@@ -24,8 +22,14 @@ function Makefile() {
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(value);
-    console.log(selectedOption);
+    var file = {
+      value,
+      selectedOption
+    };
+    axios
+      .post("http://localhost:8080/code", file)
+      .then(() => console.log("File created"))
+      .catch((err) => console.log(err));
   };
   const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const index = event.target.value;
@@ -37,12 +41,12 @@ function Makefile() {
         <option selected disabled>
           Choose one
         </option>
-        <option value="Python">Python</option>
-        <option value="C">C</option>
-        <option value="Java">Java</option>
+        <option value="py">Python</option>
+        <option value="c">C</option>
+        <option value="java">Java</option>
       </select>
-      <form action="submit" onSubmit={onSubmit}>
-        <Codebox type="text" onChange={onChange} />
+      <form method="POST" action="submit" onSubmit={onSubmit}>
+        <Codebox rows={50} cols={100} onChange={onChange} required />
         <Button>Send Code</Button>
       </form>
     </Container>
--- server/server.js
+++ server/server.js
@@ -2,14 +2,33 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+var file = [];
+
 app.listen(8080, function () {
 	console.log("listening on 8080");
 });
+
+app.use(express.json());
+var cors = require("cors");
+const { writeFile } = require("fs");
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../app/build")));
 
 app.get("/", function (request, response) {
 	response.sendFile(path.join(__dirname, "../app/build/index.html"));
+});
+
+app.post("/code", function (request, response) {
+	var newFile = {
+		text: request.body.value,
+		option: request.body.selectedOption,
+	};
+	var fs = require("fs");
+	var data = newFile.text;
+	fs.writeFileSync(`test.${newFile.option}`, data, "utf-8");
+	file.push(newFile);
+	console.log(file);
 });
 
 // React Router 사용
